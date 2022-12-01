cd8168f40b8cb5f4e1173a94769bb02233eb68f3
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-12-01 10:53:23 +0100

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./public/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      outfit: ['Outfit', 'sans-serif']
+    },
+  },
+  plugins: [],
+}
