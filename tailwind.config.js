c9774bd8ad9214993ea64e4a5c7ac96ff27db601
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-12-01 11:55:33 +0100

    Header and menu done

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ module.exports = {
     fontFamily: {
       outfit: ['Outfit', 'sans-serif']
     },
+    colors: {
+      'white': '#ffffff',
+      'darkblue': '#04152F',
+      'blue': '#1363DF',
+      'lightblue': '#EBF3FF',
+      'menu': '#F4F4F4'
+    },
   },
   plugins: [],
 }
